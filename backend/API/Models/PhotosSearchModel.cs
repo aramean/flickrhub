70b9b167a55d098535b24ee883d53fbcb2e788f0
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-02-14 15:32:05 +0100

    immutable

--- backend/API/Models/PhotosSearchModel.cs
+++ backend/API/Models/PhotosSearchModel.cs
@@ -5,45 +5,45 @@ namespace API.Models;
 public class PhotosSearchModel()
 {
     [JsonPropertyName("photos")]
-    public Photos? Photos { get; set; }
+    public Photos? Photos { get; init; }
 
     [JsonPropertyName("stat")]
-    public string? Status { get; set; }
+    public string? Status { get; init; }
 
     [JsonPropertyName("code")]
-    public int? Code { get; set; } = 200;
+    public int? Code { get; init; } = 200;
 
     [JsonPropertyName("message")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public string? Message { get; set; }
+    public string? Message { get; init; }
 }
 
 public class Photos()
 {
     [JsonPropertyName("photo")]
-    public List<Photo>? Photo { get; set; }
+    public List<Photo>? Photo { get; init; }
 
     [JsonPropertyName("page")]
-    public int Page { get; set; } = 1;
+    public int Page { get; init; } = 1;
 
     [JsonPropertyName("perpage")]
-    public int PerPage { get; set; } = 0;
+    public int PerPage { get; init; } = 0;
 
     [JsonPropertyName("total")]
-    public int Total { get; set; } = 0;
+    public int Total { get; init; } = 0;
 }
 
 public class Photo()
 {
     [JsonPropertyName("id")]
-    public string? Id { get; set; }
+    public string? Id { get; init; }
 
     [JsonPropertyName("title")]
-    public string? Title { get; set; }
+    public string? Title { get; init; }
 
     [JsonPropertyName("secret")]
-    public string? Secret { get; set; }
+    public string? Secret { get; init; }
 
     [JsonPropertyName("server")]
-    public string? Server { get; set; }
+    public string? Server { get; init; }
 }
