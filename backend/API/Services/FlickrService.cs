a35565f53904ebc753aef62bcc92e5a624715fdb
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-02-15 13:05:27 +0100

    load key from env or use secret

--- backend/API/Services/FlickrService.cs
+++ backend/API/Services/FlickrService.cs
@@ -6,10 +6,10 @@ namespace API.Services;
 public class FlickrService(HttpClient httpClient, IConfiguration configuration)
 {
   private readonly HttpClient _httpClient = httpClient;
-    private readonly string _baseUrl = configuration["FlickrApi:BaseUrl"] 
+    private readonly string _baseUrl = configuration["FlickrApi:BaseUrl"]
         ?? throw new Exception("FlickrApi:BaseUrl is missing in configuration.");
-    private readonly string _apiKey = configuration["ApiKey"] 
-        ?? throw new Exception("API key is missing in configuration.");
+    private readonly string _apiKey = Environment.GetEnvironmentVariable("FLICKR_API_KEY") ?? configuration["ApiKey"]
+        ?? throw new Exception("API key is missing.");
 
     private string CommonParameters => $"&api_key={_apiKey}&format=json&nojsoncallback=1";
 
